889a11ca27f27acc83467cda6b5e4c610a2bc141
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-07-05 09:58:58 +0300

    Finish task

--- server/app.py
+++ server/app.py
@@ -21,7 +21,7 @@ api = Api(app)
 class ClearSession(Resource):
 
     def delete(self):
-    
+
         session['page_views'] = None
         session['user_id'] = None
 
@@ -48,10 +48,29 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id if user else None
+        return make_response(jsonify(user.to_dict()), 200) if user else make_response({}, 401)
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response('', 204)
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        return make_response(user.to_dict(), 200) if user else make_response({}, 401)
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
